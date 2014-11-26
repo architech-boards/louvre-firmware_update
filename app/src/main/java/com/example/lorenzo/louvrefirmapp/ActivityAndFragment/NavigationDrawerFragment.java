65c8a4d3acfbeaf4412c5278dec9484346bfb1fc
lstradella <lstradella@bitbucket.org>
2014-11-26 12:28:17 +0100

    - Improved register info fragment with custom adapter - Other GUI improvements

--- app/src/main/java/com/example/lorenzo/louvrefirmapp/ActivityAndFragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/lorenzo/louvrefirmapp/ActivityAndFragment/NavigationDrawerFragment.java
@@ -1,4 +1,4 @@
-package com.example.lorenzo.louvrefirmapp;
+package com.example.lorenzo.louvrefirmapp.ActivityAndFragment;
 
 
 import android.app.Activity;
@@ -21,6 +21,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.example.lorenzo.louvrefirmapp.R;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
